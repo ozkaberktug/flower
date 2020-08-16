64cc1a907031a61b35ef86e032973821785d06b8
ozkaberktug <b.kaan.ozkan@gmail.com>
2020-08-16 11:08:57 +0300

    creating resources

---
+++ src/flower/resources/ResourceManager.java
@@ -0,0 +1,18 @@
+package flower.resources;
+
+import flower.App;
+
+import javax.swing.ImageIcon;
+import java.net.URL;
+
+public class ResourceManager {
+
+    public static final String ARROW_DOWN = "arrow-down.png";
+    public static final String ARROW_UP = "arrow-up.png";
+
+    public static ImageIcon getImageIcon(String path) {
+        URL ref = App.class.getResource("resources/"+path);
+        return new ImageIcon(ref);
+    }
+
+}
